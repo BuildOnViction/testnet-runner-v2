eb9642e06609de01ce9830e897473dec2d1559bd
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2020-01-22 09:52:29 +0000

    update app script

--- app.sh
+++ app.sh
@@ -11,20 +11,20 @@ echo Starting tomox-sdk ...
 cd /home/vagrant/go/src/github.com/tomochain/tomox-sdk && pm2 start ./tomox-sdk --name tomox-sdk
 cd /home/vagrant/projects/tomox-sdk-ui && pm2 start npm --name tomox-sdk-ui -- start
 
-#echo Sleep 30 seconds ...
-#sleep 30
-#echo Starting bot ...
-#cd /home/vagrant/projects/tomox-market-maker && pm2 start cmd.js --name bot_TOMOBTC -- bot TOMO-BTC
-#cd /home/vagrant/projects/tomox-market-maker && pm2 start cmd.js --name bot_ETHTOMO -- bot ETH-TOMO
-#cd /home/vagrant/projects/tomox-market-maker && pm2 start cmd.js --name bot_XRPTOMO -- bot XRP-TOMO
-#cd /home/vagrant/projects/tomox-market-maker && pm2 start cmd.js --name bot_LTCTOMO -- bot LTC-TOMO
-#cd /home/vagrant/projects/tomox-market-maker && pm2 start cmd.js --name bot_BNBTOMO -- bot BNB-TOMO
-#cd /home/vagrant/projects/tomox-market-maker && pm2 start cmd.js --name bot_ADATOMO -- bot ADA-TOMO
-#cd /home/vagrant/projects/tomox-market-maker && pm2 start cmd.js --name bot_ETCTOMO -- bot ETC-TOMO
-#cd /home/vagrant/projects/tomox-market-maker && pm2 start cmd.js --name bot_BCHTOMO -- bot BCH-TOMO
-#cd /home/vagrant/projects/tomox-market-maker && pm2 start cmd.js --name bot_EOSTOMO -- bot EOS-TOMO
-#cd /home/vagrant/projects/tomox-market-maker && pm2 start cmd.js --name bot_ETHBTC -- bot ETH-BTC 
-#cd /home/vagrant/projects/tomox-market-maker && pm2 start cmd.js --name bot_XRPBTC -- bot XRP-BTC 
-#cd /home/vagrant/projects/tomox-market-maker && pm2 start cmd.js --name bot_TOMOUSD -- bot TOMO-USD
-#cd /home/vagrant/projects/tomox-market-maker && pm2 start cmd.js --name bot_BTCUSD -- bot BTC-USD
-#cd /home/vagrant/projects/tomox-market-maker && pm2 start cmd.js --name bot_ETHUSD -- bot ETH-USD
+echo Sleep 30 seconds ...
+sleep 30
+echo Starting bot ...
+cd /home/vagrant/projects/tomox-market-maker && pm2 start cmd.js --name bot_TOMOBTC -- bot TOMO-BTC
+cd /home/vagrant/projects/tomox-market-maker && pm2 start cmd.js --name bot_ETHTOMO -- bot ETH-TOMO
+cd /home/vagrant/projects/tomox-market-maker && pm2 start cmd.js --name bot_XRPTOMO -- bot XRP-TOMO
+cd /home/vagrant/projects/tomox-market-maker && pm2 start cmd.js --name bot_LTCTOMO -- bot LTC-TOMO
+cd /home/vagrant/projects/tomox-market-maker && pm2 start cmd.js --name bot_BNBTOMO -- bot BNB-TOMO
+cd /home/vagrant/projects/tomox-market-maker && pm2 start cmd.js --name bot_ADATOMO -- bot ADA-TOMO
+cd /home/vagrant/projects/tomox-market-maker && pm2 start cmd.js --name bot_ETCTOMO -- bot ETC-TOMO
+cd /home/vagrant/projects/tomox-market-maker && pm2 start cmd.js --name bot_BCHTOMO -- bot BCH-TOMO
+cd /home/vagrant/projects/tomox-market-maker && pm2 start cmd.js --name bot_EOSTOMO -- bot EOS-TOMO
+cd /home/vagrant/projects/tomox-market-maker && pm2 start cmd.js --name bot_ETHBTC -- bot ETH-BTC
+cd /home/vagrant/projects/tomox-market-maker && pm2 start cmd.js --name bot_XRPBTC -- bot XRP-BTC
+cd /home/vagrant/projects/tomox-market-maker && pm2 start cmd.js --name bot_TOMOUSD -- bot TOMO-USD
+cd /home/vagrant/projects/tomox-market-maker && pm2 start cmd.js --name bot_BTCUSD -- bot BTC-USD
+cd /home/vagrant/projects/tomox-market-maker && pm2 start cmd.js --name bot_ETHUSD -- bot ETH-USD
